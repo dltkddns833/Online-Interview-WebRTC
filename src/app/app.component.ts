101356d41311955376a0f901060be6f88b175389
PAROMA <dltkddns833@naver.com>
2020-07-20 14:19:22 +0900

    Commit 2020-07-20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit 
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webcam';
+export class AppComponent implements AfterViewInit {
+  public constraints = {
+    video: {
+      width : {max : 640},
+      height : {max : 480},
+      facingMode: "user", // 전면 카메라
+      frameRate: { ideal: 10, max: 15 }
+    },
+    audio: false
+  }
+
+
+  @ViewChild('localVideo') localVideoTag : any
+
+
+  constructor() {
+  }
+  
+  ngAfterViewInit(){
+    if(navigator.mediaDevices != null){
+      console.log(this.localVideoTag)
+      navigator.mediaDevices.getUserMedia(this.constraints)
+      .then(stream => {
+        console.log('Adding local stream.');
+        console.log(stream)
+        let video = this.localVideoTag.nativeElement;
+        video.srcObject = stream;
+      })
+      .catch(e => {
+          console.log(e);
+          alert('카메라 접근이 허용되지 않았습니다.');
+      });
+    }
+  }
+
+  initWebRTCPage(){
+    if(navigator.mediaDevices != null){
+      console.log(this.localVideoTag)
+      navigator.mediaDevices.getUserMedia(this.constraints)
+      .then(this.gotStream)
+      .catch(function(e) {
+          console.log(e);
+          alert('카메라 접근이 허용되지 않았습니다@');
+      });
+    }
+  }
+
+  gotStream(stream){
+    let video = this.localVideoTag.nativeElement;
+    console.log('Adding local stream.');
+    console.log(video)
+    console.log(stream)
+    video.srcObject = stream;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
